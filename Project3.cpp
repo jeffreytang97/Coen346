ca7ef8e659f660ef86ffd51b3cd79c906c2e786a
boomtski <35674413+boomtski@users.noreply.github.com>
2018-03-19 12:45:21 -0400

    Project3

---
+++ Project3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+//ofstream --> Stream class to write file
+//ifstream --> Stream class to read from file
+//fstream --> Stream class to both read and write from/to file
+#include <pthread.h>
+#include <string>
+
+using namespace std;
+
+void main (){
+	ifstream file ("pbs_input.txt");
+	string PID;
+	int arrival_time;
+	int burst;
+	int priority;
+	
+	if(file.is_open()){
+		while 
+	}
+}
