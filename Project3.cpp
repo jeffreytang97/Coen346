c021bdf87f2fb5427033bb170683681a5ad13937
Jeffrey <jeffreytang97@gmail.com>
2018-03-20 09:37:29 -0400

    Essaye de trouver comment relie PID a arrival_time, burst et priority

--- Project3.cpp
+++ Project3.cpp
@@ -11,22 +11,22 @@ using namespace std;
 
 void main (){
 	ifstream file ("pbs_input.txt");
-	string* PID;
-	int arrival_time = &PID;
-	int burst = &PID;
-	int priority = &PID;
+	
 	bool flag1, flag2; //to indicate which queue of process is active or expired
 
 	vector <int> processQ1; //queues for locating processes
 	vector <int> processQ2;
-
-	/*int* processQ1;
-	processQ1 = new int[]; //creating a dynamic array with variable size
-	int* processQ2;
-	processQ2 = new int[]; //creating a dynamic array with variable size
-	*/
-	
 	vector <string> PIDVector;
+
+	struct parameters {
+		string PID;
+		int arrival_time;
+		int burst;
+		int priority;
+	};
+
+	parameters process;
+
 	//vector <int> arriveVector;
 	//vector <int> burstVector;
 	//vector <int> priorityVector;
@@ -34,6 +34,7 @@ void main (){
 	if(file.is_open()){ //If the file is opened
 		while (file >> PID >> arrival_time >> burst >> priority){
 			PIDVector.push_back(PID);
+
 			//arriveVector.push_back(arrival_time);
 			//burstVector.push_back(burst);
 			//priorityVector.push_back(priority);
