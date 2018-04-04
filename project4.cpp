a9ced46a02919a5a82724eae286aacef82f13721
Jeffrey <jeffreytang97@gmail.com>
2018-04-04 18:45:13 -0400

    project4 creation

---
+++ project4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+//ofstream --> Stream class to write file
+//ifstream --> Stream class to read from file
+//fstream --> Stream class to both read and write from/to file
+#include <string>
+#include <vector>
+#include <math.h>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <mutex>
+
+using namespace std;
+
+void main() {
+
+}
